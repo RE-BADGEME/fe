b478463cf10bf98366cf4b07e4b2393c4ec12a99
MinSeobKim <icdmiracle927@gmail.com>
2023-12-07 12:08:02 +0900

    Feat : main page 변경

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div className="w-screen h-screen bg-green-400 flex flex-col justify-center items-center text-5xl font-bold">
-      시작이 반이다.
+      시작이 반이다. 가보자
     </div>
   );
 };
