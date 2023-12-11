f7f1270f0a4395457411df44333b17ee975cdd7f
MinSeobKim <icdmiracle927@gmail.com>
2023-12-11 19:19:25 +0900

    Feat : api test 변경

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,13 @@
 import HappyAnimation from '@/components/Common/LottieAnimation/HappyAnimation';
 import HeaderTemplate from '@/components/Common/Template/HeaderTemplate';
 import Banner from '@/components/Home/Banner/Banner';
+import { getCurrentUrl } from '@/utils/getCurrentUrl';
 
 const Page = async () => {
+  const url = getCurrentUrl();
+  console.log(url);
   const fetchHello = async () => {
-    const response = await fetch('/api/items', {
+    const response = await fetch(`${url}/api/items/get`, {
       method: 'GET',
     });
 
@@ -14,13 +17,13 @@ const Page = async () => {
     return data;
   };
 
-  const text = await fetchHello();
-  console.log(text);
+  const data = await fetchHello();
+  console.log(data);
 
   return (
     <HeaderTemplate>
       <Banner />
-      {text.message}
+      {data.message}
       <HappyAnimation />
     </HeaderTemplate>
   );
